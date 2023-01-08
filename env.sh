09f3cfb08948c7d874c8c93107fe78a9938e9e5e
Micah John <micahvdm@gmail.com>
2023-01-08 21:58:23 +0200

    initial

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export DEB_ROOT=$DIR/debroot
+
+export MOD_LV2_DIR=/home/pistomp/.lv2
---
+++ jackd2/build-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+source ../env.sh
+
+PACKAGE=jack2
+VERSION=1.9.14
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc
