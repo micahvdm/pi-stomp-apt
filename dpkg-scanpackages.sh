8e067caed44a78a41d6f268c45a0a66806505ad5
Micah John <micahvdm@gmail.com>
2025-02-12 13:54:20 +0200

    add more packages to builder

---
+++ dpkg-scanpackages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+for i in $(ls); do
+    if [ "$i" = "docs" ]; then
+        continue
+    fi
+	if [ -d $i ]; then
+		cd $i
+		if [ -e SKIP ]; then
+			echo Copying $i deb file
+			cp *.deb ../docs/pool/main/
+			rm SKIP
+		else
+			echo Skipping copying $i due to no SKIP file.
+		fi
+		cd ..
+	fi
+done
+
+dpkg-scanpackages docs/pool /dev/null > docs/dists/stable/main/binary-amd64/Packages
+gzip -k -f docs/dists/stable/main/binary-amd64/Packages
+
+git add docs
+git commit -m "Updating APT package $1"
